7b68b4dcd3a4c07a93dd49f166c0a26e479fe984
Adam Ługowski <adam.lugowski@live.com>
2024-10-07 12:02:07 +0200

    enable user choice of city in Poland and handle user input

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from data import Station
 def main():
     database = Database()
     database.db_init()
-    station = Station('Wroclaw', 'pl')
-    station.save_to_db(database)
-    database.display_data()
+    city = input('Enter city name to check weather: ').strip().lower().capitalize()
+    station = Station(city, 'pl')
+    if station.is_valid_city_name(city):
+        station.save_to_db(database)
+        database.display_data()
+    else:
+        print('Invalid city name. Please try again. ')
 
 
 if __name__ == '__main__':
